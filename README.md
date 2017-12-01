69c97daef93c55f45c4d09173a14b978ce7bacbb
Dmitry Moskowski <me@corpix.ru>
2017-12-01 01:03:02 +0000

    Added transform example

--- README.md
+++ README.md
@@ -4,3 +4,18 @@ lua
 [![Build Status](https://travis-ci.org/corpix/lua.svg?branch=master)](https://travis-ci.org/corpix/lua)
 
 Helpers around https://github.com/yuin/gopher-lua
+
+## Examples
+
+Here is a simple example which shows how to transform data from Lua:
+
+``` console
+$ go run ./examples/transform/transform.go
+(map[string]interface {}) (len=1) {
+ (string) (len=7) "how you": (string) (len=5) "doin?"
+}
+(map[interface {}]interface {}) (len=2) {
+ (string) (len=7) "how you": (string) (len=5) "doin?",
+ (string) (len=5) "hello": (string) (len=5) "world"
+}
+```
---
+++ examples/transform/transform.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"github.com/davecgh/go-spew/spew"
+	lua "github.com/yuin/gopher-lua"
+
+	"github.com/corpix/lua/mapper"
+	"github.com/corpix/lua/pool"
+)
+
+func newLState() *lua.LState {
+	var (
+		l   = lua.NewState()
+		err error
+	)
+
+	err = l.DoString(
+		`
+            function transform(v)
+                v["hello"] = "world"
+                return v
+            end
+        `,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return l
+}
+
+func main() {
+	var (
+		input = map[string]interface{}{
+			"how you": "doin?",
+		}
+		output      interface{}
+		p           = pool.New(newLState)
+		l           = p.Get()
+		inputValue  lua.LValue
+		outputValue lua.LValue
+		err         error
+	)
+	defer p.Put(l)
+	defer p.Close()
+
+	inputValue, err = mapper.ToValue(input)
+	if err != nil {
+		panic(err)
+	}
+
+	err = l.CallByParam(
+		lua.P{
+			Fn:      l.GetGlobal("transform"),
+			NRet:    1,
+			Protect: true,
+		},
+		inputValue,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	outputValue = l.Get(-1)
+	l.Pop(1)
+
+	output, err = mapper.FromValue(outputValue)
+	if err != nil {
+		panic(err)
+	}
+
+	spew.Dump(
+		input,
+		output,
+	)
+}
